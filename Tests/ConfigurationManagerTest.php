34a359ee67802012915e89b4661e3336bf10aabd
sim <brown.simon.a@gmail.com>
2019-01-23 18:04:43 +0100

    Configuration manager test

---
+++ Tests/ConfigurationManagerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sherlockode\ConfigurationBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Sherlockode\ConfigurationBundle\Manager\ConfigurationManager;
+use Sherlockode\ConfigurationBundle\Parameter\ParameterDefinition;
+
+class ConfigurationManagerTest extends TestCase
+{
+    public function testConstructor()
+    {
+        $config = [
+            'path' => [
+                'type'  => 'string',
+                'label' => 'test',
+            ],
+        ];
+
+        $configurationManager = new ConfigurationManager($config);
+        $parameterDefinition = new ParameterDefinition('path', 'string', ['label' => 'test']);
+
+        $this->assertEquals($configurationManager->get('path'), $parameterDefinition);
+    }
+}
