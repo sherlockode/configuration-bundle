6c10c0a8e1a7f8109701e684550302d0b5e4c59e
Julien Chichignoud <julien.chichignoud@gmail.com>
2020-02-14 10:15:04 +0100

    Add save event for extension

--- Controller/ParameterController.php
+++ Controller/ParameterController.php
@@ -2,11 +2,14 @@
 
 namespace Sherlockode\ConfigurationBundle\Controller;
 
+use Sherlockode\ConfigurationBundle\Event\SaveEvent;
 use Sherlockode\ConfigurationBundle\Form\Type\ParametersType;
 use Sherlockode\ConfigurationBundle\Manager\ParameterManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Kernel;
 
 /**
  * Class ParameterController
@@ -24,15 +27,22 @@ class ParameterController extends AbstractController
     private $editFormTemplate;
 
     /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
+
+    /**
      * ParameterController constructor.
      *
      * @param ParameterManagerInterface $parameterManager
+     * @param EventDispatcherInterface  $eventDispatcher
      * @param string                    $editFormTemplate
      */
-    public function __construct($parameterManager, $editFormTemplate)
+    public function __construct(ParameterManagerInterface $parameterManager, EventDispatcherInterface $eventDispatcher, $editFormTemplate)
     {
         $this->parameterManager = $parameterManager;
         $this->editFormTemplate = $editFormTemplate;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     /**
@@ -51,6 +61,12 @@ class ParameterController extends AbstractController
                 $this->parameterManager->set($path, $value);
             }
             $this->parameterManager->save();
+
+            if (Kernel::VERSION_ID < 40300) {
+                $this->eventDispatcher->dispatch(SaveEvent::class, new SaveEvent());
+            } else {
+                $this->eventDispatcher->dispatch(new SaveEvent());
+            }
 
             return $this->redirectToRoute('sherlockode_configuration.parameters');
         }
---
+++ Event/SaveEvent.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Sherlockode\ConfigurationBundle\Event;
+
+class SaveEvent
+{
+
+}
